b84cb415534b16b8cb4caada401809307968776a
Hakue <adam.haldas@gmail.com>
2020-09-05 12:17:58 +0200

    Updates

---
+++ precache-manifest.5aff0e9c5292beb330d241934a68fa76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57167599c987776c6898fbcaa51ae8d9",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "d478c5ea201409f8de86",
+    "url": "/gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/gh-pages/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d478c5ea201409f8de86",
+    "url": "/gh-pages/static/js/main.5ffb6a6b.chunk.js"
+  },
+  {
+    "revision": "7861e910c76190701efa",
+    "url": "/gh-pages/static/js/runtime-main.54b604b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
